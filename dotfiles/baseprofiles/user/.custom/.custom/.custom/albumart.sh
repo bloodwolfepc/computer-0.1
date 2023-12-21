e7867a9efeeb736e34b921b2eb7d83d71f3a85c3
evilcatlawyer <browsingbuffalo478@gmail.com>
2023-12-21 01:46:57 -0600

    commit

--- dotfiles/baseprofiles/user/.custom/.custom/.custom/albumart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-album_art=$(playerctl -p tidal-hifi metadata mpris:artUrl)
-if [[ -z $album_art ]] 
-then
-   # tidal is dead, we should die to.
-   exit
-fi
-curl -s  "${album_art}" --output "/tmp/cover.jpeg"
-echo "/tmp/cover.jpeg"
